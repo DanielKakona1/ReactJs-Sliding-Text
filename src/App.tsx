8bbd278658cf1d8e360a96dacd71788d31a41c29
Danielkakona <Daniel@bridgelabs.design>
2020-09-20 18:43:54 +0200

    navigationbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <div className="container">
+      <div className="navbar">
+        <div className="logo">
+          <img src={require('./assets/logo.png')} alt="logo"/>
+        </div>
+<div className="navbarLink">
+  <p >About</p>
+  <p >Library</p>
+  <p >Blog</p>
+  <p >Contact</p>
+</div>
+
+<div className="user">
+  <p>Daiel Kakona</p>
+<img src={require('./assets/user.png')} alt="user"/>
+</div>
+      </div>
+    </div>
+  </div>
 }
 
 export default App
